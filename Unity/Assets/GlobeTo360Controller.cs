37583df2fc6d76fc1246a61119eecc9148d0153d
Jason Chen <jason.chen@yale.edu>
2018-02-12 17:38:48 +0000

    unity

---
+++ Unity/Assets/GlobeTo360Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeTo360Controller : MonoBehaviour {
+
+	public CameraObrbit cameraOrbit;
+	public Skybox skybox;
+	public Camera camera;
+	public GameObject earthGameObject;
+
+	public void Toggle360Mode(bool enable)
+	{
+		cameraOrbit.enabled = enable; //!enable
+		skybox.enabled = enable;
+		camera.clearFlags = enable ? CameraClearFlags.Skybox : CameraClearFlags.Color;
+		earthGameObject.SetActive (!enable);
+	}
+}
